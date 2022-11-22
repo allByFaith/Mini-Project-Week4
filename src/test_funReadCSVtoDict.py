f5359f5658dd463cffc992ebb6057109373cc68d
Samuel Ko <87363056+allByFaith@users.noreply.github.com>
2022-11-22 06:34:59 +0000

    Add files via upload

---
+++ src/test_funReadCSVtoDict.py
@@ -0,0 +1,28 @@
+# This is the mini-project week4 ------------------------------------------------------------------------------
+# File Name     : test_funReadCSVtoDict.py
+# Date          : 19 Nov 2022
+# Developer     : Samuel KO
+# Description   : This function receives four parameters.  The function will base
+#               : on the pass in dictionary, the record index to indicate the
+#                 position of the data that want to be updated.  Also, use the inFile
+#                 and the inHeader to help the update process
+# Input         : Pass in parameters 1) dictionary 2) file name 3) file header
+# Output        : Print the updated list to the console for verification.   No
+#                 printing if the operation fail
+# -------------------------------------------------------------------------------------------------------------
+
+import pytest
+import chkItemExist
+
+def test_isExist():
+    # Arrange
+    inList = [{'prodName': 'English Tea', 'price': 1.85},
+                {'prodName': 'Whisky', 'price': 21.99},
+                {'prodName': 'Pespi 1L', 'price': 0.99},
+                {'prodName': 'Coke Zero', 'price': 2.99}]
+    inName = 'English Tea'
+    inMenu = 'PRODUCT_MENU'
+    result = True
+
+    assert chkItemExist.isExist(inList, inName, inMenu) == result
+
